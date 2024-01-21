15c78bb8e535f581bc6b2fbacb2ccb395213b1fc
Robert Dzienio <55928400+rdzienio@users.noreply.github.com>
2024-01-22 00:06:33 +0100

    init

---
+++ src/main/java/pl/gienius/sknera/SkneraApplication.java
@@ -0,0 +1,13 @@
+package pl.gienius.sknera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkneraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkneraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gienius/sknera/SkneraApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gienius.sknera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkneraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
